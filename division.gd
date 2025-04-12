bac1898da8fdeeb52852bd32c379db96e0a7891e
Michael Hyer <michaelhy2002@gmail.com>
2025-04-12 01:14:52 -0600

    Init

---
+++ division.gd
@@ -0,0 +1,19 @@
+extends Control
+
+@onready var box1: TextEdit = $Box1
+@onready var box2: TextEdit = $Box2
+@onready var result: Label = $Result
+
+
+func _on_divide_pressed() -> void:
+    result.text = "%0.2f" % (float(box1.text) / float(box2.text))
+
+
+func _on_reset_pressed() -> void:
+    box1.text = "0"
+    box2.text = "0"
+    result.text = "0"
+
+
+func _on_next_page_pressed() -> void:
+    get_tree().change_scene_to_file("res://complete.tscn")
